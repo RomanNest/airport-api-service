95add07fc8afff6496e543dc7bf5e91e1ae01f67
rompu <rom.puh@gmail.com>
2024-08-06 10:00:40 +0300

    add capacity to airplanes

--- airport/serializers.py
+++ airport/serializers.py
@@ -63,7 +63,7 @@ class AirplaneTypeSerializer(serializers.ModelSerializer):
 class AirplaneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airplane
-        fields = ("id", "name", "rows", "seats_in_rows", "airplane_type")
+        fields = ("id", "name", "rows", "seats_in_rows", "airplane_type", "capacity")
 
 
 class AirplaneListSerializer(AirplaneSerializer):
