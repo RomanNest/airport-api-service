0b4ef07c6c8d5af294f891725f003fc38e0f5790
rompu <rom.puh@gmail.com>
2024-08-07 19:51:55 +0300

    add filtering to flight

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework import viewsets
 
 from airport.models import (
@@ -119,7 +121,46 @@ class OrderViewSet(viewsets.ModelViewSet):
 
 
 class FlightViewSet(viewsets.ModelViewSet):
-    queryset = Flight.objects.all()
+    queryset = Flight.objects.all().select_related(
+        "route__source",
+        "route__destination",
+        "airplane__airplane_type"
+    )
+
+    @staticmethod
+    def _params_to_ints(query_string):
+        """Converts a list of string IDs to a list of integers"""
+        return [int(str_id) for str_id in query_string.split(",")]
+
+    def get_queryset(self):
+        airplanes = self.request.query_params.get("airplanes")
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+        date = self.request.query_params.get("departure_time")
+
+        queryset = self.queryset
+
+        if airplanes:
+            airplanes_id = self._params_to_ints(airplanes)
+            queryset = queryset.filter(airplane__id__in=airplanes_id)
+
+        if source:
+            source_id = self._params_to_ints(source)
+            queryset = queryset.filter(route__source_id__in=source_id)
+
+        if destination:
+            destination_id = self._params_to_ints(destination)
+            queryset = queryset.filter(route__destination__id__in=destination_id)
+
+        if date:
+            departure_date = datetime.strptime(date, "%Y-%m-%d")
+            queryset = queryset.filter(
+                departure_time__year=departure_date.year,
+                departure_time__month=departure_date.month,
+                departure_time__day=departure_date.day,
+            )
+
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -127,14 +168,6 @@ class FlightViewSet(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return FlightDetailSerializer
         return FlightSerializer
-
-    def get_queryset(self):
-        queryset = self.queryset
-        if self.action == "list":
-            return queryset.select_related()
-        elif self.action == "retrieve":
-            return queryset.select_related().prefetch_related("crew")
-        return queryset
 
 
 class RouteViewSet(viewsets.ModelViewSet):
@@ -152,4 +185,3 @@ class RouteViewSet(viewsets.ModelViewSet):
         if self.action in ("list", "retrieve"):
             return queryset.select_related("source", "destination")
         return queryset
-
