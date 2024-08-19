81f567199ca6ef4450b0638cf176530b686e024a
rompu <rom.puh@gmail.com>
2024-08-19 22:07:26 +0300

    moved the SECRET_KEY to .env

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z40f(58h%l+8$8^s^aqh4b1yv%2gxyz3$jl%448ft0dy!h039m"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
